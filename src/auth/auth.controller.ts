110f5da23cb69a34a39fdf7424d833a8055d1e7b
khalilnazari <khalil.deve@gmail.com>
2024-11-24 10:12:01 +0800

    Login Part1 is done

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,6 @@
 import { Controller, Post, Body } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { LoginDto } from './dto/create-auth.dto';
+import { LoginDto } from './dto/cauth.dto';
 
 @Controller('auth')
 export class AuthController {
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,29 @@
-import { Injectable } from '@nestjs/common';
-import { LoginDto } from './dto/create-auth.dto';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { LoginDto } from './dto/cauth.dto';
+import { UserService } from 'src/user/user.service';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
-  async login(authData: LoginDto) {}
+  constructor(private readonly userService: UserService) {}
+
+  async login(authData: LoginDto) {
+    const { email, password } = authData;
+    // check if user exist
+    const isUserExist = await this.userService.findOneByEmail(email);
+    if (!isUserExist) {
+      throw new UnauthorizedException('Wrong credentials');
+    }
+    // check password is correct
+    const passwordMatched = await bcrypt.compare(
+      password,
+      isUserExist.password,
+    );
+    if (!passwordMatched) {
+      throw new UnauthorizedException('Wrong credentials');
+    }
+
+    // create auth token
+    return authData;
+  }
 }
